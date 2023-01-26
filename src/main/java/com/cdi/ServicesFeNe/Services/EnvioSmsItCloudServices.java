ab9654c7e7cc69c8b795e1afc8f2f078c672e748
williambernal1003 <84921661+williambernal1003@users.noreply.github.com>
2023-01-26 12:08:11 -0500

    Ajustes

---
+++ src/main/java/com/cdi/ServicesFeNe/Services/EnvioSmsItCloudServices.java
@@ -0,0 +1,7 @@
+package com.cdi.ServicesFeNe.Services;
+
+import com.cdi.ServicesFeNe.Entity.EnvioSmsItCloudEntity;
+
+public interface EnvioSmsItCloudServices {
+public String EnvioSms(EnvioSmsItCloudEntity entidad);    
+}
